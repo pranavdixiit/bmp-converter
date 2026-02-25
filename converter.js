c96d1be232f3b1328e600fa6457b25eaf6512833
Pranav <153559220+pranavdixiit@users.noreply.github.com>
2026-02-25 20:20:45 +0530

    Update converter.js

--- converter.js
+++ converter.js
@@ -142,11 +142,11 @@ function makeRGB565(width, height, imageData) {
     for (let x = 0; x < width; x++) {
       const i = (y * width + x) * 4;
 
-        const r = imageData[i];
+        const b = imageData[i];
 
 
       const g = imageData[i + 1];
-      const b = imageData[i + 2];
+      const r = imageData[i + 2];
 
       const rgb565 =
         ((r & 0xF8) << 8) |
@@ -203,6 +203,7 @@ resetBtn.addEventListener('click', () => {
   previewBox.classList.remove('loaded');
   fileInput.value = '';
 });
+
 
 
 
